ca673af3ff9bff5533d0ea6bc28dfb639e736696
CodeTestNedeljkovic <nikola.nedeljkovic@code.edu.rs>
2023-01-25 19:36:58 +0100

    Begging of class 27

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = 6;
+
+// /asgiuasgnasf
+// ffasfasf
+// /asgiuasgnasf / GIT blame Nikola Nedeljkovic
+// ffasfasf
+// /asgiuasgnasf
+// ffasfasf
+// /asgiuasgnasf
+// ffasfasf
+// /asgiuasgnasf / GIT blame Marko Markovic 2months ago "Change requested for date implementation"
+// ffasfasf
+// /asgiuasgnasf
+// ffasfasf
